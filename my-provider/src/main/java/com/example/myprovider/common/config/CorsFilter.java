956032092520f86909021a0ec35f21b40d32331e
twan <599054904@qq.com>
2021-01-14 10:05:52 +0800

    解决跨域

---
+++ my-provider/src/main/java/com/example/myprovider/common/config/CorsFilter.java
@@ -0,0 +1,26 @@
+package com.example.myprovider.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 解决跨域问题
+ * https://mp.weixin.qq.com/s/254H2bPRiK83-YpmKvpYOA
+ */
+@WebFilter(filterName = "CorsFilter ")
+@Configuration
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        chain.doFilter(req, res);
+    }
+}
